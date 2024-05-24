2f19c2141ab1a5bdbb1144f01689e56433c28c0f
Sujan <mail.thapasujan@gmail.com>
2024-05-24 11:02:07 +1000

    initialised an empty spring boot application for the crash course

---
+++ src/main/java/dev/sujanthapa/content_calender/ContentCalenderApplication.java
@@ -0,0 +1,13 @@
+package dev.sujanthapa.content_calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCalenderApplication.class, args);
+	}
+
+}
